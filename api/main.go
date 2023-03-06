e7b6b8359b8323d7bbeedbc99b72c7344f92b39b
Martin Gressler <martin.gressler@code.berlin>
2023-03-06 01:49:39 +0100

    implemented jwt verify middleware

--- api/main.go
+++ api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"robanohashi/api/controllers"
 	"robanohashi/persist"
+	"strings"
 
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 )
 
 func main() {
@@ -13,17 +19,60 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to connect to Redis:", err)
 	}
-	router := gin.Default()
+	r := gin.Default()
 
-	router.Use(func(c *gin.Context) {
+	r.Use(func(c *gin.Context) {
 		c.Set("db", db)
 		c.Next()
 	})
 
-	router.GET("/search", controllers.Search)
-	router.GET("/kanji/:id", controllers.GetKanji)
-	router.GET("/subject/:id/meaning_mnemonics", controllers.GetMeaningMnemonics)
-	router.GET("/radical/:id", controllers.GetRadical)
-	router.GET("/vocabulary/:id", controllers.GetVocabulary)
-	router.Run(":5000")
+	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing firebase app: %v", err)
+	}
+
+	auth, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v", err)
+	}
+
+	authorized := r.Group("")
+	authorized.Use(ValidateFirebaseToken(auth))
+
+	authorized.GET("/user", func(ctx *gin.Context) {
+		uid := ctx.MustGet("uid").(string)
+		ctx.JSON(http.StatusOK, gin.H{"uid": uid})
+	})
+
+	r.GET("/search", controllers.Search)
+	r.GET("/kanji/:id", controllers.GetKanji)
+	r.GET("/subject/:id/meaning_mnemonics", controllers.GetMeaningMnemonics)
+	r.GET("/radical/:id", controllers.GetRadical)
+	r.GET("/vocabulary/:id", controllers.GetVocabulary)
+	r.Run(":5000")
+}
+
+func ValidateFirebaseToken(auth *auth.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		bearer := c.GetHeader("Authorization")
+
+		if bearer == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthenticated"})
+			return
+		}
+
+		idToken := strings.Split(bearer, " ")[1]
+
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthenticated"})
+			return
+		}
+
+		uid := token.UID
+
+		c.Set("uid", uid)
+	}
 }
